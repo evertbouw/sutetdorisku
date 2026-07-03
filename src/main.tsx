def52460ff404660831458f3d42269145685e681
Evert Bouw <bouw@outlook.com>
2026-07-03 14:27:24 +0200

    render react

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 import { assert } from "./utils/assert";
 import { isDefined } from "./utils/isDefined";
-import { createRoot } from "react-dom/client";
 
 const node = document.getElementById("app");
 
@@ -8,4 +9,4 @@ assert(isDefined(node), "Could not find app element");
 
 const root = createRoot(node);
 
-root.render("HELLO WORLD");
+root.render(<App />);
